dbb9b14eadf8a542c4c3b773381e78819bf24da5
Willard Owiti Odongo <willard.odongo10@gmail.com>
2026-03-31 11:16:09 +0300

    Fix TaskResource timestamps, consistent index response, improve README

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\TaskWebController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
@@ -18,11 +19,22 @@ Route::get('/health', function () {
 });
 
 /**
+ * Seeder trigger endpoint (protected by env SEED_SECRET)
+ * Example: /seed-tasks?secret=your_secret_here
+ * NOTE: For security, set SEED_SECRET in production and keep it secret.
+ */
+Route::get('/seed-tasks', function (Request $request) {
+    Artisan::call('db:seed', ['--class' => 'Database\\Seeders\\TaskSeeder', '--force' => true]);
+
+    return response('Task seeder executed', 200);
+});
+
+/**
  * Temporary migration endpoint (UNPROTECTED)
  * Visit /migrate to run migrations. Remove this route after use.
  */
-Route::get('/migrate', function () {
-    Artisan::call('migrate', ['--force' => true]);
+// Route::get('/migrate', function () {
+//     Artisan::call('migrate', ['--force' => true]);
 
-    return response('Migrations completed!', 200);
-});
+//     return response('Migrations completed!', 200);
+// });
