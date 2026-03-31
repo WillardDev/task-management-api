666b83fbf66b8a46709450c9553d4051c76f8b2d
Willard Owiti Odongo <willard.odongo10@gmail.com>
2026-03-31 12:05:23 +0300

    added report endpoint

--- app/Http/Controllers/TaskWebController.php
+++ app/Http/Controllers/TaskWebController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Task;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class TaskWebController extends Controller
 {
@@ -20,9 +21,39 @@ class TaskWebController extends Controller
     {
         return view('tasks.create');
     }
-    
+
     public function show(Task $task)
     {
         return view('tasks.show', compact('task'));
+    }
+
+    public function report(Request $request)
+    {
+        $date = $request->query('date') ? Carbon::parse($request->query('date')) : Carbon::today();
+
+        $start = $date->copy()->startOfMonth()->toDateString();
+        $end = $date->copy()->endOfMonth()->toDateString();
+
+        $tasks = Task::whereBetween('due_date', [$start, $end])->get()
+            ->groupBy(function ($task) {
+                return $task->due_date->format('Y-m-d');
+            });
+
+        $tasksForJs = [];
+        foreach ($tasks as $day => $group) {
+            $tasksForJs[$day] = $group->map(function ($t) {
+                return [
+                    'id' => $t->id,
+                    'title' => $t->title,
+                    'priority' => $t->priority,
+                    'status' => $t->status,
+                ];
+            })->values()->toArray();
+        }
+
+        return view('tasks.report', [
+            'tasksByDate' => $tasksForJs,
+            'currentDate' => $date->format('Y-m-d'),
+        ]);
     }
 }
